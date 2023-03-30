f60c0df724c99f6ab267265caaed32b16c408bd3
nerocui <nerocui@outlook.com>
2023-03-30 01:38:25 +0000

    initial commit

---
+++ src/EIAU/Server/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EIAU.Server.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Handle { get; set; }
+    public string ID { get; set; }
+    public string AvatarUrl { get; set; }
+    public ICollection<ApplicationUser> Followers { get; set; }
+    public ICollection<ApplicationUser> Following { get; set; }
+    public ICollection<ApplicationUser> Blocked { get; set; }
+    public ICollection<ApplicationUser> Friends { get; set; }
+}
